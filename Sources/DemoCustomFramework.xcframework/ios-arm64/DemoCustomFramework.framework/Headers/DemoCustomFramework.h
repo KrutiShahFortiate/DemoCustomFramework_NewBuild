6de1a46750df4ced9b464894832b4c6487bf78ba
KrutiShah_Fortiate <kruti@fortiate.com>
2022-05-24 16:01:19 +0530

    version 0.0.2

---
+++ Sources/DemoCustomFramework.xcframework/ios-arm64/DemoCustomFramework.framework/Headers/DemoCustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  DemoCustomFramework.h
+//  DemoCustomFramework
+//
+//  Created by Kruti on 24/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoCustomFramework.
+FOUNDATION_EXPORT double DemoCustomFrameworkVersionNumber;
+
+//! Project version string for DemoCustomFramework.
+FOUNDATION_EXPORT const unsigned char DemoCustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoCustomFramework/PublicHeader.h>
+
+
--- Sources/DemoCustomFramework/DemoCustomFramework.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public struct SDK {
-    
-    public static func doSomeWork() {
-        print("Doing some work...")
-    }
-    
-}
